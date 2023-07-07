a8368e72162d340766238b9335bc9dac09c1b99f
Shaolong Li <shaolongli26@gmail.com>
2023-07-07 14:08:40 +0800

    add react route

---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import AddRestaurant from "../components/AddRestaurant";
+
+const Home = () => {
+    return (
+        <div>
+           <Header></Header>
+            <AddRestaurant></AddRestaurant>
+        </div>
+    )
+}
+
+export default Home;
